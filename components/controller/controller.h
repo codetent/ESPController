36b477afd8abacff8b74d8e32a9ba41ffdc47702
Milos Jovanovic <jeyem228@hotmail.com>
2020-01-17 13:50:14 +0100

    ESP_RC: change joystick to controller

--- components/controller/controller.h
+++ components/controller/controller.h
@@ -1,5 +1,5 @@
-#ifndef __JOYSTICK_H__
-#define __JOYSTICK_H__
+#ifndef __CONTROLLER_H__
+#define __CONTROLLER_H__
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -12,10 +12,10 @@
 /* -------------------------------------------------------------------------- */
 
 typedef enum {
-    JOYSTICK_POS_NEUTRAL,
-    JOYSTICK_POS_1,
-    JOYSTICK_POS_2
-} joystick_ch_position_t;
+    CONTROLLER_POS_NEUTRAL,
+    CONTROLLER_POS_1,
+    CONTROLLER_POS_2
+} controller_ch_position_t;
 
 typedef struct {
     uint32_t x_value;
@@ -36,14 +36,14 @@ typedef struct {
     } config;
 
     bool _initialized;
-} joystick_t;
+} controller_t;
 
 typedef struct {
     uint32_t x_delta;
     uint32_t y_delta;
-    joystick_ch_position_t x_position;
-    joystick_ch_position_t y_position;
-} joystick_position_t;
+    controller_ch_position_t x_position;
+    controller_ch_position_t y_position;
+} controller_position_t;
 
 
 /* -------------------------------------------------------------------------- */
@@ -51,30 +51,30 @@ typedef struct {
 /* -------------------------------------------------------------------------- */
 
 /**
- * @brief Initialize the peripherals to which the joystick is connected
+ * @brief Initialize the peripherals to which the controller is connected
  * 
- * @param joystick Structure which holds the joystick information
+ * @param controller Structure which holds the controller information
  * @return esp_err_t 
  */
-esp_err_t joystick_configure(joystick_t *joystick);
+esp_err_t controller_configure(controller_t *controller);
 
 /**
- * @brief Read raw values of the joystick and store it in the given structure
+ * @brief Read raw values of the controller and store it in the given structure
  * 
- * @param joystick Structure which holds the joystick information
+ * @param controller Structure which holds the controller information
  * @return esp_err_t 
  */
-esp_err_t joystick_read_raw(joystick_t *joystick);
+esp_err_t controller_read_raw(controller_t *controller);
 
 /**
- * @brief Calculate the position of the joystick with the values stored in the given structure
+ * @brief Calculate the position of the controller with the values stored in the given structure
  * 
- * @param joystick Structure which holds the joystick information
+ * @param controller Structure which holds the controller information
  * @param threshold Threshold for out-of-center detection
- * @param position Resulting joystick position structure
+ * @param position Resulting controller position structure
  * @return esp_err_t 
  */
-esp_err_t joystick_calc_pos(joystick_t *joystick, uint32_t threshold, joystick_position_t *position);
+esp_err_t controller_calc_pos(controller_t *controller, uint32_t threshold, controller_position_t *position);
 
 
-#endif // __JOYSTICK_H__
+#endif // __CONTROLLER_H__
--- main/main.c
+++ main/main.c
@@ -4,18 +4,18 @@
 #include <stdbool.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "joystick.h"
+#include "controller.h"
 
 
 /* -------------------------------------------------------------------------- */
 /*                                   DEFINES                                  */
 /* -------------------------------------------------------------------------- */
 
-#define JOYSTICK_X_ADC_CH ADC_CHANNEL_0
-#define JOYSTICK_Y_ADC_CH ADC_CHANNEL_3
-#define JOYSTICK_ARM_GPIO GPIO_NUM_34
-#define JOYSTICK_THR_DOWN_GPIO GPIO_NUM_35
-#define JOYSTICK_THR_UP_GPIO GPIO_NUM_32
+#define CONTROLLER_X_ADC_CH ADC_CHANNEL_0
+#define CONTROLLER_Y_ADC_CH ADC_CHANNEL_3
+#define CONTROLLER_ARM_GPIO GPIO_NUM_34
+#define CONTROLLER_THR_DOWN_GPIO GPIO_NUM_35
+#define CONTROLLER_THR_UP_GPIO GPIO_NUM_32
 
 
 /* -------------------------------------------------------------------------- */
@@ -24,30 +24,30 @@
 
 static void task_interface(void *args)
 {
-    joystick_t joystick = {
+    controller_t controller = {
         .config = {
-            .x_adc_channel = JOYSTICK_X_ADC_CH,
-            .y_adc_channel = JOYSTICK_Y_ADC_CH,
-            .arm_gpio_num = JOYSTICK_ARM_GPIO,
-            .thr_up_gpio_num = JOYSTICK_THR_UP_GPIO,
-            .thr_down_gpio_num = JOYSTICK_THR_DOWN_GPIO 
+            .x_adc_channel = CONTROLLER_X_ADC_CH,
+            .y_adc_channel = CONTROLLER_Y_ADC_CH,
+            .arm_gpio_num = CONTROLLER_ARM_GPIO,
+            .thr_up_gpio_num = CONTROLLER_THR_UP_GPIO,
+            .thr_down_gpio_num = CONTROLLER_THR_DOWN_GPIO 
         }
     };
-    joystick_position_t position = {0U};
+    controller_position_t position = {0U};
 
     // Configure periphery
     adc1_config_width(ADC_WIDTH_BIT_12);
-    joystick_configure(&joystick);
+    controller_configure(&controller);
 
     while (1U) 
     {
-        // Get joystick position
-        joystick_read_raw(&joystick);
-        joystick_calc_pos(&joystick, 100U, &position);
+        // Get controller position
+        controller_read_raw(&controller);
+        controller_calc_pos(&controller, 100U, &position);
 
         // Print result
         printf("X: %d - %d Y: %d - %d ARM: %d, THR_DOWN: %d, THR_UP: %d\n", position.x_position, position.x_delta, position.y_position, position.y_delta,
-                                                 joystick.arm_value, joystick.thr_down_value, joystick.thr_up_value);
+                                                 controller.arm_value, controller.thr_down_value, controller.thr_up_value);
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 
